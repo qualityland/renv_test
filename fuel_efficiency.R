5ba5044659fc0b65ecca6f2a4ccc18fa9a2a03c1
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2021-02-05 17:36:08 +0100

    labels corrected.

--- fuel_efficiency.R
+++ fuel_efficiency.R
@@ -6,5 +6,5 @@ ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy, color = class)) +
   labs(title = 'Fuel Efficiency',
        subtitle = 'on the Highway',
-       x = 'engine displacement (l)',
-       y = 'efficiency (miles per gallon)')
+       x = 'Engine Displacement (l)',
+       y = 'Fuel Efficiency (miles per gallon)')
